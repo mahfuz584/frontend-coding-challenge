949072c554461ae67e9f7b5008be40b8b8ff7ab4
Your Name <mahfuzhasan587@gmail.com>
2024-12-23 23:00:16 +0600

    challenges sloved

--- src/app/user-agent/page.tsx
+++ src/app/user-agent/page.tsx
@@ -1,7 +1,14 @@
+import { UserAgentProvider } from "@/components/providers/userAgentProvider";
 import { UserAgent } from "@/views/userAgent";
+import { headers } from "next/headers";
 
 const UserAgentRoot = () => {
-  return <UserAgent />;
+  const userAgent = headers().get("user-agent") || "Unknown";
+  return (
+    <UserAgentProvider userAgent={userAgent}>
+      <UserAgent />
+    </UserAgentProvider>
+  );
 };
 
 export default UserAgentRoot;
